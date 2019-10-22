dcee6713dfcae06e7612b4be35f971051de2ec55
Praveengouda Patil <pspatil@usc.edu>
2019-10-21 23:17:01 -0700

    Rate Limiter and Hit Counter Design

---
+++ ShortestDistanceBetweenWords.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class ShortestDistanceBetweenWords {
+	
+	
+	private static int shortestDistance1(String[] words,String word1,String word2) {
+		
+		
+		int m=-1;
+		int n=-1;
+		int min = Integer.MAX_VALUE;
+		for(int i=0;i<words.length;i++) {
+			
+			if(word1.equals(words[i])) {
+				m = i;
+				if(n!=-1)
+					min = Math.min(min, m-n);
+			}
+			else if(word2.equals(words[i])) {
+				n=i;
+				if(m!=-1)
+					min=Math.min(min, n-m);
+			}
+		}
+		
+		return min;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- UberRateLimiter.java
+++ UberRateLimiter.java
@@ -3,130 +3,85 @@ package leetcode;
 import java.util.Queue;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class UberRateLimiter {
 	
 	
-	private static class HitCounter {
-		
-		public Queue<Long> q;
-		public HitCounter() {
-			q = new LinkedList<>();
-		}
-		
-		public boolean hit(long timestamp) {
-			
-			while(!q.isEmpty() && (timestamp - q.peek())>=3)
-				q.poll();
-			
-			if(q.size() < 5) {
-				q.add(timestamp);
-				return true;
-			}
-			return false;
-		}
-	}
+	    public static class RateLimiter {
+	        private final int REQUEST_LIMIT = 3;
+	        private final long TIME_LIMIT = 1000L; // 1000L -> 10 seconds, 1000 -> 1 second
+	    	
+	        public class HitCounter {
+	        	
+	            Queue<Long> q = null;
+	            public HitCounter() {
+	                q = new LinkedList<Long>();
+	            }
+
+	            public boolean hit(long timestamp) {
+	                while(!q.isEmpty() && timestamp-q.peek() >= TIME_LIMIT)
+	                	q.poll();
+	                
+	                if(q.size() < REQUEST_LIMIT)
+	                {
+	                    q.offer(timestamp); 
+	                    return true;
+	                }
+	                
+	                return false;
+	            }
+	        }
+	        
+	        private final Map<String, HitCounter> clientHitMap = new HashMap<String, HitCounter>();
+	        
+	        public boolean isAllow(String client_id) {
+	            HitCounter h = clientHitMap.get(client_id);
+	            
+	            long curTime = System.currentTimeMillis();
+	            
+	            if(h == null) {
+	                h = new HitCounter();
+	                clientHitMap.put(client_id, h);
+	            } 
+	            
+	            return h.hit(curTime);
+	        }        
+	    }    
+	    
+	    public static void main(String[] args) {
+	    	
+	    	
+	        RateLimiter limiter = new RateLimiter();
+	        System.out.println("Client1 " + limiter.isAllow("Client1"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        System.out.println("Sleeping for 1 second");
+	        try {
+	            java.lang.Thread.sleep(1000); 
+	        } catch (InterruptedException ex) {
+	            System.out.println(ex);
+	        }
+	        System.out.println("Client1 " + limiter.isAllow("Client1"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	        
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client2 " +limiter.isAllow("Client2"));
+	        System.out.println("Client1 " +limiter.isAllow("Client1"));
+	    }
+	    
+	    
 	
-	public static HashMap<String,HitCounter> map = new HashMap<>();
-	public static boolean isAllow(String id) {
-		
-		long curTime = System.currentTimeMillis();
-		
-		if(!map.containsKey(id)) {
-			HitCounter h = new HitCounter();
-			h.hit(curTime);
-			return true;
-		}
-		else {
-			HitCounter h = map.get(id);
-			return h.hit(curTime);
-		}
-	}
-	public static void main(String[] args) {
-		
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("2"));
-		System.out.println(isAllow("3"));
-		System.out.println(isAllow("4"));
-		System.out.println(isAllow("5"));
-		System.out.println(isAllow("6"));
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("1"));
-		System.out.println(isAllow("1"));
-	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- 
- import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class RateLimiter {
-	public int REQUEST_LIMIT = 100; 
-	public Long TIME_LIMIT = 1000L; 
-	public class HitCounter {
-		public Queue<Long> queue;
-		public HitCounter() {
-			queue = new LinkedList<>();	
-		}
-		public boolean hit(long timestamp) {
-
-			while (!queue.isEmpty() && queue.peek() - timestamp >= TIME_LIMIT) queue.poll();
-			if (queue.size() < 500) {
-				queue.add(timestamp); return true;
-			}
-			return false;
-		}
-	}
-	public HashMap<String, HitCounter> clientTimeStampMap = new HashMap<>();
-	public boolean isAllow(String clientId) {
-		long currTime = System.currentTimeMillis();
-		if (!clientTimeStampMap.containsKey(clientId)) {
-			HitCounter h = new HitCounter();
-			h.hit(currTime); return true;
-		} else {
-			HitCounter h = clientTimeStampMap.get(clientId);
-			return h.hit(currTime);
-		}
-	}
-	
-}*/
